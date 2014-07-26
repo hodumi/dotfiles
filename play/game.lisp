724bb98b4eca6eaaa790113a047c6290a0839c8d
hodumi <hodumi@yusuke-pc.(none)>
2014-07-26 14:31:55 +0900

    game整理

--- play/game.lisp
+++ play/game.lisp
@@ -1,32 +1,16 @@
 
-;; fnを実行
-(defun dofn (fn &rest args)
-  (lambda () (apply fn args) nil))
+(lambda (args)
+  (lambda ()
+    (body)
+    (next-state)))
 
 
-;; 指定回数実行
-(defun times (time fn &rest args)
-  (label
-   (tms (tm)
-	(lambda ()
-	  (if (zerop tm)
-	      nil
-	    (progn
-	      (apply fn args)
-	      (tms (1- tm))))))
-   (tms time)))
+(lambda ()
+  (body)
+  (lambda ()
+    (body)
+    (lambda ()
+      (body)
+      #'func)))
 
-;; 待機
-(defun wait (time)
-  (label
-   (w (tm)
-      (lambda ()
-	(if (zerop tm)
-	    t
-	  (w (1- tm)))))
-   (w time)))
 
-;; 処理を記述
-(defun process (&rest list)
-  
-  )
