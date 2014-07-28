62d01bf8286526f5c08f82519fad7cd90c06dc10
kudo <y.kudo@hoc-net.jp>
2014-07-28 13:38:08 +0900

    一画面の上下移動をM-pとM-nに変更

--- .emacs.d/inits/05-keybind.el
+++ .emacs.d/inits/05-keybind.el
@@ -3,3 +3,8 @@
 ;; C-hをBackspaceに変更
 (keyboard-translate ?\C-h ?\C-?)
 
+; M-pを「一画面上に移動」に変更
+(define-key global-map "\M-p" 'scroll-down-command)
+
+; M-nを「一画面下に移動」に変更
+(define-key global-map "\M-n" 'scroll-up-command)
