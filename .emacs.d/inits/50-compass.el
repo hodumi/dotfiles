67a77687e41961c2a26a4ca0828120b0e68b55ab
hodumi <hodumi@yusuke-pc.(none)>
2014-08-05 18:55:40 +0900

    compass-create追加
    [32mdirectory[0m sass/
    [32mdirectory[0m stylesheets/
    [32m   create[0m config.rb
    [32m   create[0m sass/screen.scss
    [32m   create[0m sass/print.scss
    [32m   create[0m sass/ie.scss
    [31m[0m[32m   create[0m stylesheets/ie.css
    [31m[0m[32m   create[0m stylesheets/print.css
    [31m[0m[32m   create[0m stylesheets/screen.css
    
    *********************************************************************
    Congratulations! Your compass project has been created.
    
    You may now add and edit sass stylesheets in the sass subdirectory of your project.
    
    Sass files beginning with an underscore are called partials and won't be
    compiled to CSS, but they can be imported into other sass stylesheets.
    
    You can configure your project by editing the config.rb configuration file.
    
    You must compile your sass stylesheets into CSS when they change.
    This can be done in one of the following ways:
      1. To compile on demand:
         compass compile [path/to/project]
      2. To monitor your project for changes and automatically recompile:
         compass watch [path/to/project]
    
    More Resources:
      * Website: http://compass-style.org/
      * Sass: http://sass-lang.com
      * Community: http://groups.google.com/group/compass-users/
    
    To import your new stylesheets add the following lines of HTML (or equivalent) to your webpage:
    <head>
      <link href="/stylesheets/screen.css" media="screen, projection" rel="stylesheet" type="text/css" />
      <link href="/stylesheets/print.css" media="print" rel="stylesheet" type="text/css" />
      <!--[if IE]>
          <link href="/stylesheets/ie.css" media="screen, projection" rel="stylesheet" type="text/css" />
      <![endif]-->
    </head>を実行する関数を追加した。

--- .emacs.d/inits/50-compass.el
+++ .emacs.d/inits/50-compass.el
@@ -1,14 +1,19 @@
 ; -*- Mode: Emacs-Lisp ; Coding: utf-8 -*-
 
-(defun compass-command (name buffer-or-name command)
-  (let ((default-directory (locate-dominating-file default-directory "config.rb")))
-    (start-process-shell-command name buffer-or-name (concat "compass " command))
-    (switch-to-buffer buffer-or-name)
+(defun compass-command (command &optional directory)
+  (let ((default-directory (if directory
+			       directory
+			     (locate-dominating-file default-directory "config.rb"))))
+    (start-process-shell-command "compass" "*compass*" (concat "compass " command))
+    (switch-to-buffer "*compass*")
 ;    (set-buffer-process-coding-system 'sjis 'sjis) 
     ))
 
+(defun compass-create (directory)
+  (interactive "DDirectory")
+  (compass-command "create" directory))
+
 (defun compass-watch ()
   (interactive)
-  (compass-command "compass" "*compass-watching*" "watch")
+  (compass-command "watch")
   )
-
