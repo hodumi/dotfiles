f384da9a301898bd50b0427bd96d24315b32fa8c
kudo <y.kudo@hoc-net.jp>
2014-08-19 09:12:55 +0900

    C-dをdelete-forward-charに変更

--- .emacs.d/inits/05-keybind.el
+++ .emacs.d/inits/05-keybind.el
@@ -3,11 +3,16 @@
 ;; C-hをBackspaceに変更
 (keyboard-translate ?\C-h ?\C-?)
 
-; M-pを「一画面上に移動」に変更
-(define-key global-map "\M-p" 'scroll-down-command)
+;; 選択削除を可能にするため、
+;; C-dを`delete-char`から`delete-forward-char`(<del>と同じ関数)に変更
+(define-key global-map (kbd "C-d") 'delete-forward-char)
 
-; M-nを「一画面下に移動」に変更
-(define-key global-map "\M-n" 'scroll-up-command)
+;; M-pを「一画面上に移動」に変更
+(define-key global-map (kbd "M-p") 'scroll-down-command)
 
-; C-S-kを「一行削除」に変更
-(define-key global-map "\C-K" 'kill-whole-line)
+;; M-nを「一画面下に移動」に変更
+(define-key global-map (kbd "M-n") 'scroll-up-command)
+
+;; C-S-kを「一行削除」に変更
+(define-key global-map (kbd "C-S-k") 'kill-whole-line)
+
