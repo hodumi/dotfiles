cd119cc823464f747811d649ffd5427851583964
kudo <y.kudo@hoc-net.jp>
2014-08-19 15:30:23 +0900

    rainbow-delimiters導入

---
+++ .emacs.d/inits/40-rainbow-delimiters.el
@@ -0,0 +1,5 @@
+; -*- Mode: Emacs-Lisp ; Coding: utf-8 -*-
+
+(require 'rainbow-delimiters)
+(global-rainbow-delimiters-mode t)
+;; (custom-set-faces '(rainbow-delimiters-depth-1-face ((t (:foreground "#7f8c8d")))))
