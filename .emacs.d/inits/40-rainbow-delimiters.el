f765bcc404b9493e497e842d322e4a800958f2e8
kudo <y.kudo@hoc-net.jp>
2014-08-20 16:50:54 +0900

    対応する開きカッコがない閉じカッコを強調表示するよう設定

--- .emacs.d/inits/40-rainbow-delimiters.el
+++ .emacs.d/inits/40-rainbow-delimiters.el
@@ -3,3 +3,13 @@
 (require 'rainbow-delimiters)
 (global-rainbow-delimiters-mode t)
 ;; (custom-set-faces '(rainbow-delimiters-depth-1-face ((t (:foreground "#7f8c8d")))))
+
+(set-face-attribute
+ 'rainbow-delimiters-mismatched-face nil
+ :foreground "#e60033"
+ :height 1.5)
+
+(set-face-attribute
+ 'rainbow-delimiters-unmatched-face nil
+ :foreground "#e60033"
+ :height 1.5)
