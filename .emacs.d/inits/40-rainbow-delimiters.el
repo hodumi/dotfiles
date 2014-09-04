748e64da372d1bf9b2d80855952c12947aa67372
kudo <y.kudo@hoc-net.jp>
2014-09-05 07:11:49 +0900

    バグが発見されたため、rainbow-delimiterを使用不可にした。

--- .emacs.d/inits/40-rainbow-delimiters.el
+++ .emacs.d/inits/40-rainbow-delimiters.el
@@ -1,19 +1,19 @@
 ; -*- Mode: Emacs-Lisp ; Coding: utf-8 -*-
 
-(require 'rainbow-delimiters)
-(global-rainbow-delimiters-mode t)
-;; (custom-set-faces '(rainbow-delimiters-depth-1-face ((t (:foreground "#7f8c8d")))))
+;; (require 'rainbow-delimiters)
+;; (global-rainbow-delimiters-mode t)
+;; ;; (custom-set-faces '(rainbow-delimiters-depth-1-face ((t (:foreground "#7f8c8d")))))
 
-(set-face-attribute
- 'rainbow-delimiters-mismatched-face nil
- :foreground "#e60033"
- :height 1.5
- :weight 'bold
- )
+;; (set-face-attribute
+;;  'rainbow-delimiters-mismatched-face nil
+;;  :foreground "#e60033"
+;;  :height 1.5
+;;  :weight 'bold
+;;  )
 
-(set-face-attribute
- 'rainbow-delimiters-unmatched-face nil
- :foreground "#e60033"
- :height 1.5
- :weight 'bold
- )
+;; (set-face-attribute
+;;  'rainbow-delimiters-unmatched-face nil
+;;  :foreground "#e60033"
+;;  :height 1.5
+;;  :weight 'bold
+;;  )
