afcf671ac1b971a9c43bd7aeaa4c67d9cc8d47e6
kudo <y.kudo@hoc-net.jp>
2014-09-11 15:46:08 +0900

    05-keybind.elをbind-keyに対応、それに伴いファイル名を60-keybind.elに変更
    bind-key.el読み込み後にkeybind設定をするため

--- .emacs.d/inits/60-keybind.el
+++ .emacs.d/inits/60-keybind.el
@@ -1,22 +1,23 @@
 ; -*- Mode: Emacs-Lisp ; Coding: utf-8 -*-
 
+
 ;; C-hをBackspaceに変更
 (keyboard-translate ?\C-h ?\C-?)
 
 ;; 選択削除を可能にするため、
 ;; C-dを`delete-char`から`delete-forward-char`(<del>と同じ関数)に変更
-(define-key global-map (kbd "C-d") 'delete-forward-char)
+(bind-key (kbd "C-d") 'delete-forward-char)
 
 ;; M-pを「一画面上に移動」に変更
-(define-key global-map (kbd "M-p") 'scroll-down-command)
+(bind-key (kbd "M-p") 'scroll-down-command)
 
 ;; M-nを「一画面下に移動」に変更
-(define-key global-map (kbd "M-n") 'scroll-up-command)
+(bind-key (kbd "M-n") 'scroll-up-command)
 
 ;; 元の「一画面上下移動」を無効化
-(define-key global-map (kbd "M-v") nil)
-(define-key global-map (kbd "C-v") nil)
+(unbind-key (kbd "M-v"))
+(unbind-key (kbd "C-v"))
 
 ;; C-S-kを「一行削除」に変更
-(define-key global-map (kbd "C-S-k") 'kill-whole-line)
+(bind-key (kbd "C-S-k") 'kill-whole-line)
 
