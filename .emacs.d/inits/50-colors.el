4eba7709d8c92e054b45632bff9cd124819b19ba
hodumi <ajsmithy00@gmail.com>
2014-10-07 21:28:42 +0900

    add commands complementary-color and inversion-color

--- .emacs.d/inits/50-colors.el
+++ .emacs.d/inits/50-colors.el
@@ -2,8 +2,16 @@
 
 ;;; 16進数 -> 10進数のリスト
 (defun rgb->r-g-b (rgb)
-  
+   
   )
+
+
+;; RGB表示
+(defun message-rgb (r g b)
+  (message (format "R:%s G:%s B:%s" r g b))
+  (sit-for 3))
+
+
 
 ;; 補色(illustrator流)
 (defun complementary-color-for-illustrator (r g b)
@@ -12,15 +20,19 @@
 
 ;; 補色
 (defun complementary-color (r g b)
-  (complementary-color-for-illustrator r g b))
+  (interactive "nR: \nnG: \nnB: ")
+  (apply #'message-rgb (complementary-color-for-illustrator r g b)))
 
 
 ;; 反転色
 (defun inversion-color (r g b)
-  (list (- 255 r)
-	(- 255 g)
-	(- 255 b)))
+  (interactive "nR: \nnG: \nnB: ")
+  (apply #'message-rgb (list (- 255 r)
+			     (- 255 g)
+			     (- 255 b))))
 
-(defun message-rgb (r g b)
-  
-  )
+
+
+
+
+
