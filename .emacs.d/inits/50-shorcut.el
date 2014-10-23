7939e0646308d09d169dac938aa8d2041c917517
kudo <y.kudo@hoc-net.jp>
2014-10-23 11:58:12 +0900

    Windowsで、explorerを表示する関数`explorer`を追加

--- .emacs.d/inits/50-shorcut.el
+++ .emacs.d/inits/50-shorcut.el
@@ -2,6 +2,18 @@
 
 (require 'cl-lib)
 
+(defun explorer (&optional path)
+  "引数があればそのパスの、引数が省略されていれば現在のバッファのファイルを、explorerで開きます。"
+  (interactive)
+  (setq path (expand-file-name (or path (buffer-file-name))))
+  (cond
+    ((not (file-exists-p path))
+     (message "path %s isn't exist" path))
+    (t
+     (let ((dos-path (replace-regexp-in-string "/" "\\\\" path)))
+       (w32-shell-execute "open" "explorer.exe" (concat "/select," dos-path))))))
+
+
 (defun cygstart (args)
   "Execute \"cygstart\"."
   (interactive "Mcommand: ")
