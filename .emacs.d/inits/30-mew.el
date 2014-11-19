125f306702033c301d4c8fef579e1989db9de610
kudo <y.kudo@hoc-net.jp>
2014-11-19 19:28:22 +0900

    setting mew

--- .emacs.d/inits/30-mew.el
+++ .emacs.d/inits/30-mew.el
@@ -5,3 +5,5 @@
 (setq mew-pop-user "y.kudo@hoc-net.jp")  ;; (user-login-name)
 (setq mew-pop-server "mail.hoc-net.jp")    ;; if not localhost
 
+(setq mew-use-cached-passwd t)
+(setq mew-use-master-passwd t)
