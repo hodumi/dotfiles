24df8a407cb01dc50eb549a460970918f0bf3b6d
hodumi <ajsmithy00@gmail.com>
2014-11-26 19:46:51 +0900

    minecraft forge用関数修正

--- .emacs.d/inits/50-mcforge.el
+++ .emacs.d/inits/50-mcforge.el
@@ -2,12 +2,14 @@
 
 (defun mcf-run-task (name buffer-or-name command switch-p)
   (let ((default-directory (locate-dominating-file default-directory "gradlew")))
-    (start-process-shell-command name buffer-or-name (concat "cmd /c \"gradlew " command " \""))
-    (if switch-p (switch-to-buffer buffer-or-name))
+    (start-process-shell-command name buffer-or-name (concat "./gradlew " command ))
+    (when switch-p 
+      (switch-to-buffer buffer-or-name)
+      (erase-buffer))
     (set-buffer-process-coding-system 'sjis 'sjis)
     ))
 
 (defun mcf-run-client ()
   (interactive)
-  (mcf-run-task "mcf:run-client" "mcf-client" "runClient" t))
+  (mcf-run-task "mcf:run-client" "*mcf-client*" "runClient" t))
 
