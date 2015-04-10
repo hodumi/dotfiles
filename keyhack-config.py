03a4aa44101e5551ea289e67aa8503d56acaeac8
Kudo Yusuke <y.kudo@hoc-net.jp>
2015-04-10 19:46:50 +0900

    keyhackの設定ファイルの管理開始

---
+++ keyhack-config.py
@@ -0,0 +1,48 @@
+﻿import sys
+import os
+import datetime
+
+import pyauto
+from keyhac import *
+
+def configure(keymap):
+
+    # --------------------------------------------------------------------
+    # config.py編集用のテキストエディタの設定
+
+    # プログラムのファイルパスを設定 (単純な使用方法)
+    if 1:
+        keymap.editor = "emacsclientw"
+
+    # 呼び出し可能オブジェクトを設定 (高度な使用方法)
+    if 0:
+        def editor(path):
+            shellExecute( None, "notepad.exe", '"%s"'% path, "" )
+        keymap.editor = editor
+
+    # --------------------------------------------------------------------
+    # 表示のカスタマイズ
+
+    # フォントの設定
+    keymap.setFont( "Migu 1M", 12 )
+
+    # テーマの設定
+    keymap.setTheme("black")
+
+    # --------------------------------------------------------------------
+
+    # 無変換->Alt
+    keymap.replaceKey( "(29)", "Alt" )
+
+    # 変換->Shift
+    keymap.replaceKey( "(28)", "Shift" )
+
+    # ひらがな/カタカナ -> Shift
+    # keymap.replaceKey( "(242)", "Shift" )
+
+    # SandS (Space and Shift)
+    keymap_global = keymap.defineWindowKeymap()
+    keymap.replaceKey("Space", "RShift")
+    keymap_global["O-RShift"] = "Space"
+    keymap_global["C-RShift"] = "C-Space"
+  
