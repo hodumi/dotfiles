d6a30ad084a447703f62fadb78d4d4f90c1c0e69
Kudo Yusuke <y.kudo@hoc-net.jp>
2015-04-13 12:33:06 +0900

    keyhackのC-RShiftをC-Spaceに変換する機能を削除
    Redo(C-_)時に選択されるから

--- keyhack-config.py
+++ keyhack-config.py
@@ -44,5 +44,5 @@ def configure(keymap):
     keymap_global = keymap.defineWindowKeymap()
     keymap.replaceKey("Space", "RShift")
     keymap_global["O-RShift"] = "Space"
-    keymap_global["C-RShift"] = "C-Space"
+    # keymap_global["C-RShift"] = "C-Space"
   
