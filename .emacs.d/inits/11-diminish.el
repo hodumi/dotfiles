8b0879e0d5de2a5b4a81ead6e51734391e306786
Kudo Yusuke <y.kudo@hoc-net.jp>
2015-08-20 11:00:47 +0900

    diminishの対象にabbrevを追加

--- .emacs.d/inits/11-diminish.el
+++ .emacs.d/inits/11-diminish.el
@@ -7,11 +7,13 @@
 
 
 
-(safe-diminish "rainbow-mode" 'rainbow-mode)
-(safe-diminish "undo-tree" 'undo-tree-mode)
-(safe-diminish "yasnippet" 'yas-minor-mode)
+(safe-diminish "abbrev" 'abbrev-mode)
 (safe-diminish "auto-complete" 'auto-complete-mode)
 (safe-diminish "flycheck" 'flycheck-mode)
-(safe-diminish "helm-mode" 'helm-mode)
 (safe-diminish "google-this" 'google-this-mode)
+(safe-diminish "helm-mode" 'helm-mode)
+(safe-diminish "rainbow-mode" 'rainbow-mode)
+(safe-diminish "undo-tree" 'undo-tree-mode)
 (safe-diminish "whitespace" 'global-whitespace-mode)
+(safe-diminish "yasnippet" 'yas-minor-mode)
+
