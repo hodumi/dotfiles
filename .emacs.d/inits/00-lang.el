9be5adc82df5b182b29d77b09e93664f34597de4
hodumi <ajsmithy00@gmail.com>
2015-09-06 16:58:31 +0900

    org-scheduleの曜日文字化け解消に、locale-coding-systemを初期値に設定

--- .emacs.d/inits/00-lang.el
+++ .emacs.d/inits/00-lang.el
@@ -1,14 +1,14 @@
-; -*- Mode: Emacs-Lisp ; Coding: utf-8 -*-
+;; ; -*- Mode: Emacs-Lisp ; Coding: utf-8 -*-
 
-;; デフォルト言語
+;; ;; デフォルト言語
 (set-language-environment "Japanese")
 
-;; デフォルトの文字コード
+;; ;; デフォルトの文字コード
 (set-default-coding-systems 'utf-8-dos)
 (prefer-coding-system 'utf-8-dos)
-(setq locale-coding-system 'utf-8)
+;; (setq locale-coding-system 'utf-8) ; これがあるとorg-scheduleで曜日が文字化けするのでコメントアウト
 
-;; ファイルパスの文字コード
+;; ;; ファイルパスの文字コード
 (setq default-file-name-coding-system 'japanese-cp932-dos)
 
 
