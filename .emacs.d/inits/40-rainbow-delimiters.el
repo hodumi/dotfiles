dd1e6c4d8f76be8edc8c4f3f73320a34170c6eeb
Kudo Yusuke <y.kudo@hoc-net.jp>
2015-09-10 17:35:32 +0900

    rainbow-delimitersの色を強調するよう設定

--- .emacs.d/inits/40-rainbow-delimiters.el
+++ .emacs.d/inits/40-rainbow-delimiters.el
@@ -1,24 +1,19 @@
 ; -*- Mode: Emacs-Lisp ; Coding: utf-8 -*-
 
-;;  (require 'rainbow-delimiters)
-;; ;; (global-rainbow-delimiters-mode t)
-;;  ;; (custom-set-faces '(rainbow-delimiters-depth-1-face ((t (:foreground "#7f8c8d")))))
+;; rainbow-delimiters を使うための設定
+(require 'rainbow-delimiters)
+(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
 
-;; (set-face-attribute
-;;  'rainbow-delimiters-mismatched-face nil
-;;  :foreground "#e60033"
-;;  :height 1.5
-;;  :weight 'bold
-;;  )
+;; 括弧の色を強調する設定
+(require 'cl-lib)
+(require 'color)
+(defun rainbow-delimiters-using-stronger-colors ()
+  (interactive)
+  (cl-loop
+   for index from 1 to rainbow-delimiters-max-face-count
+   do
+   (let ((face (intern (format "rainbow-delimiters-depth-%d-face" index))))
+    (cl-callf color-saturate-name (face-foreground face) 30))))
+(add-hook 'emacs-startup-hook 'rainbow-delimiters-using-stronger-colors)
 
-;; (set-face-attribute
-;;  'rainbow-delimiters-unmatched-face nil
-;;  :foreground "#e60033"
-;;  :height 1.5
-;;  :weight 'bold
-;;  )
 
-;; (defun my-rainbow-delimiters-hook ()
-;;   (raingow-delimiters-mode 1))
-
-;; (add-hook 'web-mode 'my-rainbow-delimiters-hook)
