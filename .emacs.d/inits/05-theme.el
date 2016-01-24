9e92bf02783e993eeb5382f969d866ad25f58dd3
hodumi <ajsmithy00@gmail.com>
2016-01-24 18:48:44 +0900

    ウィンドウ透過設定をhodumi-themeに移動

--- .emacs.d/inits/05-theme.el
+++ .emacs.d/inits/05-theme.el
@@ -1,13 +1,7 @@
 ; -*- Mode: Emacs-Lisp ; Coding: utf-8 -*-
 
-;; アクティブ時 90%  非アクティブ時 85%
-(setq default-frame-alist
-      (append (list
-               '(alpha . (90 85))
-               ) default-frame-alist))
-
 (add-to-list 'custom-theme-load-path "~/hodumi-theme/")
-(load-theme 'hodumi)
+(load-theme 'hodumi t)
 (initialize-hodumi-theme)
 
 ;; generic-x追加
