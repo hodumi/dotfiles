4705a887f714fe90aa57067009b4a0cf0b8bf936
Kudo Yusuke <y.kudo@hoc-net.jp>
2016-07-21 19:15:35 +0900

    置換の正規表現エンジンにpcre2elを使用するよう変更

--- .emacs.d/inits/40-visual-regexp.el
+++ .emacs.d/inits/40-visual-regexp.el
@@ -1,3 +1,11 @@
 ; -*- Mode: Emacs-Lisp ; Coding: utf-8 -*-
 
-(global-set-key (kbd "M-%") 'vr/query-replace)
+;; (global-set-key (kbd "M-%") 'vr/query-replace)
+
+
+(require 'pcre2el)
+(setq vr/engine 'pcre2el)
+(require 'visual-regexp-steroids)
+
+(bind-key "M-%" 'vr/query-replace)
+;(bind-key "M-%" 'vr/replace)
