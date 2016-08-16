0094755900a7cd2857625e5d96efa41118fd950c
Kudo Yusuke <y.kudo@hoc-net.jp>
2016-08-17 08:33:06 +0900

    melpaのURLを変更

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,8 +4,7 @@
 ;;; package
 
 (require 'package)
-
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 ;(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (package-initialize)
 
