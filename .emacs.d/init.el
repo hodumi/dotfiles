855e0c6b5a94afe11ce1c0173f59d3ad909cee1b
Kudo Yusuke <y.kudo@hoc-net.jp>
2017-02-05 16:03:04 +0900

    Packageダウンロード関連を変更

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,10 +3,15 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; package
 
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-;(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-(package-initialize)
+
+(require 'package) ;; You might already have this line
+
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/"))
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize) ;; You might already have this line
 
 
 ;; Installed check & Install init-loader
