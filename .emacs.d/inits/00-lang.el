41186529beafe01b2cdf8dc9375139982b501f7d
Kudo Yusuke <y.kudo@hoc-net.jp>
2017-02-05 16:01:43 +0900

    キーボード、ターミナルの文字コードをShift-JISに変更

--- .emacs.d/inits/00-lang.el
+++ .emacs.d/inits/00-lang.el
@@ -6,6 +6,8 @@
 ;; ;; デフォルトの文字コード
 (set-default-coding-systems 'utf-8-dos)
 (prefer-coding-system 'utf-8-dos)
+(set-keyboard-coding-system 'cp932)
+(set-terminal-coding-system 'cp932)
 ;; (setq locale-coding-system 'utf-8) ; これがあるとorg-scheduleで曜日が文字化けするのでコメントアウト
 
 ;; ;; ファイルパスの文字コード
