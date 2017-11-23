f9a31608ee12900c4bed9e4b2e2333df6161f6f2
Kudo Yusuke <y.kudo@hoc-net.jp>
2017-11-23 09:08:17 +0900

    magitのローカルな設定を41-magit-local.elに移動

---
+++ .emacs.d/inits/40-magit.el
@@ -0,0 +1,11 @@
+; -*- Mode: Emacs-Lisp ; Coding: utf-8 -*-
+
+(require 'magit)
+
+; magit-git-executable は、41-magit-local.el に置く
+
+
+;; branch.description を 表示
+(magit-add-section-hook 'magit-status-sections-hook
+                        'magit-insert-branch-description
+                        nil t)
