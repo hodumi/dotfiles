20ab5bd070a86f1ffeeb25b6296769ff32b51f93
Kudo Yusuke <y.kudo@hoc-net.jp>
2018-03-09 18:23:57 +0900

    magitのcommit時バッファエンコードを設定

--- .emacs.d/inits/40-magit.el
+++ .emacs.d/inits/40-magit.el
@@ -9,3 +9,7 @@
 (magit-add-section-hook 'magit-status-sections-hook
                         'magit-insert-branch-description
                         nil t)
+
+(add-hook 'git-commit-mode-hook
+          '(lambda ()
+             (set-buffer-file-coding-system 'utf-8)))
