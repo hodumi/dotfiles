dfb8baa3a146c97332ee65cef94526827092886b
Kudo Yusuke <y.kudo@hoc-net.jp>
2018-09-20 09:05:12 +0900

    popwinのバッファポップアップ関数をspecial-display-functionに変更

--- .emacs.d/inits/30-popwin.el
+++ .emacs.d/inits/30-popwin.el
@@ -1,12 +1,11 @@
 ; -*- Mode: Emacs-Lisp ; Coding: utf-8 -*-
 
 (require 'popwin)
-(setq display-buffer-function 'popwin:display-buffer)
+(setq special-display-function 'popwin:special-display-popup-window)
 
 (setq popwin:popup-window-position 'bottom)
 
 
 ; eshellをpopwinで出すように設定
 (add-to-list  'popwin:special-display-config '("^\\*eshell.*\\*$" :regexp t :height 50 ))
-
-
+(add-to-list 'special-display-regexps "^\\*eshell.*\\*$")
