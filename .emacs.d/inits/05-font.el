449387fdb58a2be5f83bdc8482fdef8834a326b6
Kudo Yusuke <y.kudo@hoc-net.jp>
2019-06-19 09:56:51 +0900

    デフォルトフォントをUtataneに変更

--- .emacs.d/inits/05-font.el
+++ .emacs.d/inits/05-font.el
@@ -1,6 +1,10 @@
 ; -*- Mode: Emacs-Lisp ; Coding: utf-8 -*-
 
-(set-face-attribute 'default nil 
-		    :family "Migu 1M"
-		    :height 120)
+;; (set-face-attribute 'default nil 
+;; 		    :family "Migu 1M"
+;; 		    :height 120)
 
+
+(set-face-attribute 'default nil 
+		    :family "Utatane"
+		    :height 120)
