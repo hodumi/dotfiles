81435d9f9f9d55c50b0374049193797f777a2d71
Kudo Yusuke <y.kudo@hoc-net.jp>
2020-01-10 18:41:54 +0900

    plantumlで、日本語が使えない不具合を設定

--- .emacs.d/inits/40-plantuml-mode.el
+++ .emacs.d/inits/40-plantuml-mode.el
@@ -1,5 +1,8 @@
 
 (setq plantuml-jar-path (expand-file-name "~/.plantuml/plantuml.jar"))
+(setq plantuml-default-exec-mode 'jar)
+(setq plantuml-output-type "png")
+(setq plantuml-jar-args '("-charset" "UTF-8" "-Dfile.encoding" "UTF-8" ))
 
 (require 'ob-plantuml)
 (setq org-plantuml-jar-path plantuml-jar-path)
